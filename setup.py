de4026360b10cc45a4a10473d41d882d23def587
Hamidreza Eivazi <hamidreza.eivazi.kourabbaslou@tu-clausthal.de>
2024-03-27 15:33:24 +0100

    Initial version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='kpca_deeponet',
+    name='kpca-deeponet',
     version='0.1.0',
     packages=find_packages(),
     author='Hamidreza Eivazi',
